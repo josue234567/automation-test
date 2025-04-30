8fc559a3e8560ef34800bf5995b74f3d3747999f
josue234567 <josue.arizaga.67@est.ucacue.edu.ec>
2025-04-30 18:37:37 -0500

    configuracin pipe line

---
+++ src/test/java/com/example/automation/_test/AutomationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automation._test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
